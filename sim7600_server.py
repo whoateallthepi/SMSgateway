4c31f8a59d5b63bc573dbf74397b5d765b2bfcfa
Tom Cooper <tom@peakmedia.info>
2024-12-26 15:42:46 +0000

    Implement config file

--- sim7600_server.py
+++ sim7600_server.py
@@ -22,6 +22,10 @@ from time import sleep
 
 from atlib import *
 
+from configparser import ConfigParser
+
+from config import config
+
 def initialise():
     '''
     Establish the shared Queue object and start the threads
@@ -34,8 +38,8 @@ def initialise():
     t1.start()
     t2.start()
 
-def send (message):
-    gsm = GSM_Device("/dev/ttyAMA0")
+def send (message, gsm_device):
+    gsm = GSM_Device(gsm_device)
 
     status = gsm.send_sms(message['number'].strip('+'), message['message'])
 
@@ -46,10 +50,13 @@ def send (message):
 
 def messages_in (q):
     # Create and connect to socket
+    
+    sms_params = config()
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 8001))
+    server_socket.bind((sms_params['listen'], int(sms_params['socket'])))
     server_socket.listen(1)
-    print('T1: Server listening on localhost:8001')
+    print("T1: Server listening on {}:{}".format(sms_params['listen'], int(sms_params['socket'])))
     while True:
         print('T1: waiting....')
         client_socket, addr = server_socket.accept()
@@ -63,7 +70,7 @@ def messages_in (q):
             q.put(m)
 
         ## Send a response to the client
-        response_dict = {'message': 'Hello, client!'}
+        response_dict = {'message': 'Message processed'}
         response_data = json.dumps(response_dict).encode()
         client_socket.sendall(response_data)
 
@@ -71,6 +78,8 @@ def messages_in (q):
     return
 
 def messages_out(q):
+    sms_params = config()
+    gsm_device = sms_params['gsm_device']
     while True:
         if q.empty():
             print("T2: Empty queue ... sleeping")
@@ -80,7 +89,7 @@ def messages_out(q):
                 entry = q.get()
                 print("\nT2: Processing queue entry")
                 print(entry)
-                send(entry)
+                send(entry, gsm_device)
 
     return
 
